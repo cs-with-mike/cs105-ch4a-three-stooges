609cfd378ab84687b16d2b97067983e8fbb84184
Levi <lwicks@westmont.edu>
2023-10-01 03:32:59 -0700

    fixed typo in tokki.sh

--- tokki.sh
+++ tokki.sh
@@ -15,4 +15,4 @@ sudo apt install lua5.3
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
 echo "Running Tokki ..."
 
-lua mytokki.lua sample.tk > out.txt 
+lua tokki.lua sample.tk > out.txt 
