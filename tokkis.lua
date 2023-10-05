a3e6826611b00e32c17797a36212f620a7305027
dponcedeleonwestmont <dponcedeleon@westmont.edu>
2023-10-05 15:43:35 -0700

    Changed error function into a print statement

--- tokkis.lua
+++ tokkis.lua
@@ -90,7 +90,7 @@ function factor()
                                 error()
                         end
                 else
-                        error()
+                        print("Error")
 
                 
                 end
