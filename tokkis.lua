4247190f9a1510d697a0d15dc19a8f76c2e9d621
dponcedeleonwestmont <dponcedeleon@westmont.edu>
2023-10-11 16:08:59 -0700

    Pushing current version of Lua

--- tokkis.lua
+++ tokkis.lua
@@ -3,6 +3,7 @@
 
 -- Global declarations
 -- Variables
+count = 0
 nextToken = ""
 f = assert(io.open(arg[1], "r")); -- attempts to open file, throws error if nil
 
@@ -25,87 +26,124 @@ lookupTable = {
 function lex()
         -- bypass whitespace
         if char == " " then
-                repeat
-                        char = f:read(1)
-                until not (char == " ")
+            repeat
+                char = f:read(1)
+            until not (char == " ")
         end
-
-        local lexeme = "";
-
-        if char:match("%a") then -- if the next character is a letter
-                nextToken = "IDENT"
-                repeat -- loop until end of lexeme (when the next char is not a letter or digit)
-                        lexeme = lexeme .. char;
-                        char = f:read(1);
-                        if char == nil then
-                                break
-                        end
-                until not (char:match("%a") or char:match("%d"))
-        elseif char:match("%d") then -- if the next character is a digit
-                nextToken = "INT_LIT";
-                repeat -- loop until end of lexeme (when the next character is not a digit)
-                        lexeme = lexeme .. char;
-                        char = f:read(1);
-                        if char == nil then
-                                break
-                        end
-                until not (char:match("%d"))
-        else -- use the lookup table
-                lexeme, nextToken = char, lookupTable[char];
-                char = f:read(1);
-        end
-        print(string.format("%s [ %s ]", nextToken, lexeme));
-end
-
-function expr()
-        print("Enter <expr>")
-        term()
-
-        while nextToken == ADD_OP or nextToken == SUB_OP do
-                lex()
-                term()
-        end
-end
-
-function term()
-        print("Enter <term>")
-        factor()
-        while nextToken == MULT_OP or nextToken == DIV_OP do
-                lex()
-                factor()
-        end
-end
-
-function factor()
-        print("Enter <factor>")
-        if nextToken == IDENT or nextToken == INT_LIT then
-                lex()
-        else
-                if nextToken == LEFT_PAREN then
-                        lex()
-                        expr()
-                        if nextToken ==  RIGHT_PAREN then
-                                lex()
-                        else
-                                print("error")
-                        end
-                else
-                        print("error")
+    
+        local lexeme = ""
+    
+        if char == nil then
+            nextToken = "EOF"
+        elseif char:match("%a") then -- if the next character is a letter
+            nextToken = "IDENT"
+            repeat -- loop until the end of the lexeme (when the next char is not a letter or digit)
+                lexeme = lexeme .. char
+                char = f:read(1)
+                if char == nil then
+                    break
                 end
+            until not (char:match("%a") or char:match("%d"))
+        elseif char:match("%d") then -- if the next character is a digit
+            nextToken = "INT_LIT"
+            repeat -- loop until the end of the lexeme (when the next character is not a digit)
+                lexeme = lexeme .. char
+                char = f:read(1)
+                if char == nil then
+                    break
+                end
+            until not (char:match("%d"))
+        else -- use the lookup table
+            nextToken = lookupTable[char]
+            lexeme = char
+            char = f:read(1)
+        end
+    
+        if nextToken ~= "EOF" then
+            print(string.format("%s [ %s ]", nextToken, lexeme))
+        else
+                nextToken = "EOF"
+                print(string.format("%s [ %s ]", nextToken, nextToken))
+        end
+    end
+    
+    -- ...
+    
+function expr(depth)
+        local str = ""
+        for i = 1, depth do
+                str = str .. ">"
+        end
+        print(str, "expr")
+        term(depth + 1)
+    
+        while nextToken == "ADD_OP" or nextToken == "SUB_OP" do
+            lex()
+            term(depth + 1)
+        end
+        
+        str = ""
+        for i = 1, depth do
+                str = str .. "<"
+        end
+        print(str, "expr")
+    end
+    
+function term(depth)
+        local str = ""
+        for i = 1, depth do
+                str = str .. ">"
+        end
+        print(str, "term")
+        factor(depth + 1)
+        while nextToken == "MULT_OP" or nextToken == "DIV_OP" do
+            lex()
+            factor(depth + 1)
         end
 
-        print("Exit <factor>")
-
-end
-
+        local str = ""
+        for i = 1, depth do
+                str = str .. "<"
+        end
+        print(str, "term")
+    end
+    
+function factor(depth)
+        local str = ""
+        for i = 1, depth do
+                str = str .. ">"
+        end
+        print(str, "factor")
+        if nextToken == "IDENT" or nextToken == "INT_LIT" then
+            lex()
+        else
+            if nextToken == "LEFT_PAREN" then
+                lex()
+                expr(depth)
+                if nextToken == "RIGHT_PAREN" then
+                    lex()
+                else
+                    print("error")
+                end
+            else
+                print("error")
+            end
+        end
+         str = ""
+        for i = 1, depth do
+                str = str .. "<"
+        end
+        print(str, "factor")
+    end
+    
 
 
 -- main loop
 function main ()
         char = f:read(1);
-        lex();
-        expr();
+        lex()
+        expr(1)
 end
 
-main();
-f:close();
+main()
+f:close()
