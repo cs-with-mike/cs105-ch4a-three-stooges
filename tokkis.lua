4adcd654a38f792785d30a52f645da083237b89b
dponcedeleonwestmont <dponcedeleon@westmont.edu>
2023-10-12 10:40:03 -0700

    t

--- tokkis.lua
+++ tokkis.lua
@@ -128,7 +128,7 @@ function factor(depth)
                 if nextToken == "RIGHT_PAREN" then
                     lex(depth)
                 else
-                    print("| Error - invalid tokki syntax at: " .. depth)
+                    print("| Error - invalid tokki syntax at: " .. lexeme)
                     for i = 1, depth do
                         print("<")
                     end
