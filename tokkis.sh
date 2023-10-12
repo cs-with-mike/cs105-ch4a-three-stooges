fa831318acfb20c1be1f1c31a819f6d30865f4fe
dponcedeleonwestmont <dponcedeleon@westmont.edu>
2023-10-11 17:31:15 -0700

    Pushing changes to sh to run

---
+++ tokkis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Authors:
+#  - Levi Wicks, lwicks@westmont.edu
+#  - David Ponce De Leon, dponcedeleon@westmont.edu
+#  - Alexis Leon, alleon@westmont.edu
+
+# Run any commands necessary to set up your language's runtime environment here.
+# If the runtime is expected to be present on Ubuntu by default, then do nothing.
+echo "Setting up runtime ..."
+
+# I'm checking Python interpreter's version here just as a placeholder.
+sudo apt install lua5.4
+
+# Run your Tokki lexer, passing in the first command line argument directly to the lexer.
+# Any output to STDOUT should be directed to a text output file titled "out.txt."
+echo "Running Tokki ..."
+
+lua tokkis.lua $1 > out.parser.txt 
+
+echo "FILE *************************************" 
+
+cat out.parser.txt
+
+echo "END FILE *********************************"
