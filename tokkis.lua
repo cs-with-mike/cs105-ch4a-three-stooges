3ce9bafaa2f1f876f1a983428526c300a6b5898f
dponcedeleonwestmont <dponcedeleon@westmont.edu>
2023-10-12 20:02:02 -0700

    test

--- tokkis.lua
+++ tokkis.lua
@@ -123,11 +123,11 @@ function factor(depth)
                     lex(depth)
                 else
                     print("| Error - invalid tokki syntax at: " .. depth)
-                    return
+                    goto exit
                 end
             else
                 print("| Error - ")
-                return
+                goto exit
             end
         end
         str = ""
